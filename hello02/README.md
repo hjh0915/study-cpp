27b06d5422b7fd5ae2a4493dc99d173b5b382c8a
hjh0915 <623299157@qq.com>
2021-08-24 13:20:04 +0800

    实现函数分离

---
+++ hello02/README.md
@@ -0,0 +1,8 @@
+函数分离
+=======
+需要先定义函数
+int add(int x, int y);
+
+测试编译
+=======
+g++ -std=c++17 -o test test.cpp
---
+++ hello02/hello_02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// 声明一个add函数
+int add(int x, int y);
+
+int main() {
+    cout << "sum: ";
+    cout << add(2, 3);
+    cout << "\n";
+
+    cout << "sum: " << add(2, 3) << "\n";
+
+    cout << "sum: " << cout.width(10) << add(2, 3) << endl;
+
+    return 0;
+}
+
+int add(int x, int y) {
+    return x+y;
+}
